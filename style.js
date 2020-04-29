e46ad5e3ba369a128d50cacadcc48189c3955859
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-04-29 15:31:40 -0700

    first command

---
+++ style.js
@@ -0,0 +1,60 @@
+///Age
+var answers = prompt("How old are you?");
+
+if (answers > 23) {
+    confirm("Because you're " + answers + ", you are older than me.")
+} else if (answers < 23){
+    confirm("Because you're " + answers + ", you are younger than me.")
+} else if (answers = 23){
+    confirm("Because you're " + answers + ", we are the same age.")
+}
+
+
+///Birthday
+var bday = prompt("Was I born on March 19, 1997?").toLowerCase();
+
+if (bday = "yes") {
+    confirm("YOU ARE CORRECT!")
+} else if (bday = "no") {
+    confirm("Sorry, I was born on March 19, 1997.")
+}
+
+
+///Sport
+var bday = prompt("Did I play Division I basketball?").toLowerCase();
+
+if (bday = "yes") {
+    confirm("YOU ARE CORRECT!")
+} else if (bday = "no") {
+    confirm("Sorry, I did with a fullride athletic scholarship.")
+}
+
+
+///Brother
+var bday = prompt("Do I have three brother?").toLowerCase();
+
+if (bday = "no") {
+    confirm("YOU ARE CORRECT!")
+} else if (bday = "yes") {
+    confirm("Sorry, I only have two brothers.")
+}
+
+
+///Dad
+var bday = prompt("Did my dad play in the NFL?").toLowerCase();
+
+if (bday = "yes") {
+    confirm("YOU ARE CORRECT!")
+} else if (bday = "no") {
+    confirm("Sorry, he actually did for the Seahawks and the Lions.")
+}
+
+
+///Coding
+var bday = prompt("Have I been coding for two years?").toLowerCase();
+
+if (bday = "no") {
+    confirm("YOU ARE CORRECT!")
+} else if (bday = "yes") {
+    confirm("Sorry, I just started in March 2020.")
+}
