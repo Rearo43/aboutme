2caeb1094f4eb568b0c13a04c299bfa6e02fcc14
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-04-29 23:13:48 -0700

    changed last question

--- style.js
+++ style.js
@@ -1,73 +1,73 @@
-// ///Age
-// var answers = prompt("How old are you?");
+///Age
+var answers = prompt("How old are you?");
 
-// if (answers > 23) {
-//     confirm("Because you're " + answers + ", you are older than me.")
-// } else if (answers < 23){
-//     confirm("Because you're " + answers + ", you are younger than me.")
-// } else if (answers === 23){
-//     confirm("Because you're " + answers + ", we are the same age.")
-// }
-
-
-// ///Sport
-// var bday = prompt("Did I play Division I basketball?").toLowerCase();
-
-// if (bday === "yes") {
-//     confirm("YOU ARE CORRECT!")
-// } else if (bday === "no") {
-//     confirm("Sorry, I did with a fullride athletic scholarship.")
-// }
+if (answers > 23) {
+    confirm("Because you're " + answers + ", you are older than me.")
+} else if (answers < 23){
+    confirm("Because you're " + answers + ", you are younger than me.")
+} else if (answers === 23){
+    confirm("Because you're " + answers + ", we are the same age.")
+}
 
 
-// ///Dad
-// var bday = prompt("Did my dad play in the NFL?").toLowerCase();
+///Sport
+var bday = prompt("Did I play Division I basketball?").toLowerCase();
 
-// if (bday === "yes") {
-//     confirm("YOU ARE CORRECT!")
-// } else if (bday === "no") {
-//     confirm("Sorry, he actually did for the Seahawks and the Lions.")
-// }
-
-
-// ///Brother
-// var bday = prompt("Do I have three brother?").toLowerCase();
-
-// if (bday === "no") {
-//     confirm("YOU ARE CORRECT!")
-// } else if (bday === "yes") {
-//     confirm("Sorry, I only have two brothers.")
-// }
+if (bday === "yes") {
+    confirm("YOU ARE CORRECT!")
+} else if (bday === "no") {
+    confirm("Sorry, I did with a fullride athletic scholarship.")
+}
 
 
-// ///Coding
-// var bday = prompt("Have I been coding for two years?").toLowerCase();
+///Dad
+var bday = prompt("Did my dad play in the NFL?").toLowerCase();
 
-// if (bday === "no") {
-//     confirm("YOU ARE CORRECT!")
-// } else if (bday === "yes") {
-//     confirm("Sorry, I just started in March 2020.")
-// }
-
-
-// ///Birthday
-// var bday = prompt("Was I born on March 19, 1997?").toLowerCase();
-
-// if (bday === "yes") {
-//     confirm("YOU ARE CORRECT!")
-// } else if (bday === "no") {
-//     confirm("Sorry, I was born on March 19, 1997.")
-// }
+if (bday === "yes") {
+    confirm("YOU ARE CORRECT!")
+} else if (bday === "no") {
+    confirm("Sorry, he actually did for the Seahawks and the Lions.")
+}
 
 
-///Favorite Number
+///Brother
+var bday = prompt("Do I have three brother?").toLowerCase();
+
+if (bday === "no") {
+    confirm("YOU ARE CORRECT!")
+} else if (bday === "yes") {
+    confirm("Sorry, I only have two brothers.")
+}
+
+
+///Coding
+var bday = prompt("Have I been coding for two years?").toLowerCase();
+
+if (bday === "no") {
+    confirm("YOU ARE CORRECT!")
+} else if (bday === "yes") {
+    confirm("Sorry, I just started in March 2020.")
+}
+
+
+///Birthday
+var bday = prompt("Was I born on March 19, 1997?").toLowerCase();
+
+if (bday === "yes") {
+    confirm("YOU ARE CORRECT!")
+} else if (bday === "no") {
+    confirm("Sorry, I was born on March 19, 1997.")
+}
+
+
+/Favorite Number
 for(i = 0; i < 4; i++){
 
     var answer = prompt("Can you guess my favorite number? Here's a hint it's between 10 and 52.");
 
     var i;
     var b = (answer > 33 && answer < 43)
-    var c = (answer < 52 && answer > 43)
+    var c = (answer < 53 && answer > 43)
 
         if (answer === "43") {
             confirm("YOU GOT IT! 43 is my jersey number!")
