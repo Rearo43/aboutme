a1b2cb3dd00177a5875788d5a164b850c7737909
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-06-11 23:25:07 -0700

    working on breaking up css

--- js/app.js
+++ js/app.js
@@ -1,19 +1,48 @@
 'use strict';
 
 let companyName = [];
+// let whyArr = [];
 
 $(document).ready(function(){
-  $.ajax('companyNames.json').then(data => {
-    data.forEach(things =>{
-      render(things);
+  $.ajax('companyNames.json').then(companyJson => {
+    companyJson.forEach(eachCompany =>{
+      render(eachCompany);
+      renderPage(eachCompany);
 
     });
+    console.log('done');
+
   })
+
 });
 
+
 function render(info){
-  $('.company').append(`<option value="${info.name}">${info.name}</option>`);
-  companyName.push(info.name);
+  $('#company').append(`<option class="${info.name}">${info.name}</option>`);
+  companyName.push(info.name, info.why);
+
 }
+
+function renderPage(whyWork) {
+  $('select').change(function(){
+
+    $('select option:selected').each(function(){
+      // $('p').text('');
+      let $companyChoice = $('select option:selected').val();
+
+      $('.yours').text($companyChoice);
+      //   if($companyChoice ===)
+      ///find the word in the arry
+      ///return index number
+      ///add 1 to the index number 
+      ///return new index number
+      $('.pTag').text(whyWork.why);
+    });
+
+  })
+    .trigger('change');
+
+}
+
 
 
